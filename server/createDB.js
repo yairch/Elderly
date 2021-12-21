dc7b9b1cff5bd2b03680af92bd7df2062417d1da
dolevdr <dolevdr95@gmail.com>
2021-12-21 17:17:04 +0200

    update connection ip

--- server/createDB.js
+++ server/createDB.js
@@ -1,6 +1,6 @@
 var mongodb=require('mongodb');  
 var MongoClient=mongodb.MongoClient;  
-var url='mongodb://localhost:27017/';  
+var url='mongodb://127.0.0.1:27017/';  
 MongoClient.connect(url,function(error, databases){// use for to connect to the databases  
     if(error){  
         throw error;  
