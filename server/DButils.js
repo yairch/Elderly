d1a11fb485ec3a7aad9d5ea2ccb3964e9501dca7
yairch <yairelidorc@wix.com>
2021-12-28 21:38:54 +0200

    chore: created db and collections

--- server/DButils.js
+++ server/DButils.js
@@ -1,15 +1,59 @@
-const sql = require('mysql');
+const {MongoClient} = require('mongodb');
 
 const config = {
-	user: 'root',
-	password: 'Minmax2Uybu!',
 	host: 'localhost',
-	database: 'elderly',
-	options: {
-		encrypt: true,
-		enableArithAbort: true
-	}
+	database: {
+		name:'Elderly',
+		url:"mongodb://127.0.0.1:27017/"
+	},
 };
+
+exports.createDatabase = async () => {
+	console.log('Tries to create database...')
+	const client = new MongoClient(config.url);
+	try{
+		await client.connect()
+
+		// creates DB with given name if not already exists
+		const dbObject = client.db(config.database.name);
+		console.log(`Database was created with name of ${config.database.name}`);  
+
+		return dbObject;
+	}
+	catch(error){
+		console.error('Database creation failed');
+		console.error(error);
+	}
+	finally {
+		client.close();  
+	}
+	
+}
+
+exports.createCollection = async (collectionName) => {
+	
+	const client = new MongoClient(config.url);
+	try{
+		await client.connect()
+
+		const db = client.db(config.database.name);
+
+		await db.createCollection(collectionName);
+		console.log(`Successfully created collection ${collectionName}`);  
+	}
+	catch(error){
+		console.error(`Failed creating collection ${collectionName}`);
+		console.error(error);
+	}
+	finally {
+		client.close();  
+	}
+}
+
+
+// exports.
+
+/*
 
 const con = new sql.createPool(config);
 
@@ -79,3 +123,5 @@ exports.convertVolunteerDetailsFromDB = function (records) {
 	});
 	return records;
 };
+
+*/
