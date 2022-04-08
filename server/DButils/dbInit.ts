b1db04f592a7e928f771e9ff300becd9ec9ed412
yairch <yairelidorc@wix.com>
2022-04-08 12:15:12 +0300

    builtin admin on init

--- server/DButils/dbInit.ts
+++ server/DButils/dbInit.ts
@@ -1,9 +1,12 @@
 import { collectionIds } from '../constants/collectionsIds';
+import { UserRole } from '../types/user';
 import {createDatabase, createCollection} from './services'
+import { insertUser } from './user';
 
 export const init = async () => {
     await createDatabase();
     await createCollection(collectionIds.users);
+    await insertUser('admin', '$2b$05$ou04etToRigixhRJJHAgV.VA5AT4ym1q1t1V6WsBYWEZlUPJaykNO', UserRole.Admin, '')
 }
 
 init();
