87dd7a1d3a03e7bf60ddb8e92543e40abc65e81b
dolevdr <dolevdr95@gmail.com>
2022-05-23 20:43:52 +0300

    add elderly logo

--- src/components/LoginForm1.js
+++ src/components/LoginForm1.js
@@ -3,29 +3,31 @@ import * as Cookies from 'js-cookie';
 import Modal from './modal/Modal';
 import { GoogleLogin, GoogleLogout } from 'react-google-login';
 import axios from 'axios';
+import images from "../resources/images.jpg"
 import './registrationForms/RegistrationForm.css';
 import { fetchMeetingsFullDetails, loginCheck } from '../services/server';
 import { getCurrentWebSocket } from '../services/notifacationService';
 // import { filterMeetings } from '../server.js';
-import {meetingFields, usersFields} from "../constants/collections";
-import {userTypes} from '../constants/userTypes';
-import {UserRole} from '../types/user'
-import {pullFromApi, SetCookie, DeleteCookie, hasCookie } from './CookieManager.js';
-const CLIENT_ID =  "454610489364-66snjbuq568fgvrluepjrusgjv8u1juv.apps.googleusercontent.com";//process.env.REACT_APP_CLIENT_ID;
+import { meetingFields, usersFields } from "../constants/collections";
+import { userTypes } from '../constants/userTypes';
+import { UserRole } from '../types/user'
+import { pullFromApi, SetCookie, DeleteCookie, hasCookie } from './CookieManager.js';
+const CLIENT_ID = "454610489364-66snjbuq568fgvrluepjrusgjv8u1juv.apps.googleusercontent.com";//process.env.REACT_APP_CLIENT_ID;
 
 function LoginForm(props) {
 	const [user, setUser] = useState({ haslogin: false, accessToken: '' });
 	const [modal, setModal] = useState(false);
 	const [mess, setMess] = useState('');
+	const [isElderly, setElderly] = useState(false);
 	// const [password, setPass] = useState('')
 	// const [message, setMessage] = useState('')
 	// const [modalisOpen, setModalisOpen] = useState(false)
 
 	const today = new Date().getTime()
-  	// console.log(today)
-  	const bucketDay= 86400000
-  	// const bucketMonth= 30*bucketDay
-  	const bucketWeek= 7*bucketDay
+	// console.log(today)
+	const bucketDay = 86400000
+	// const bucketMonth= 30*bucketDay
+	const bucketWeek = 7 * bucketDay
 	// constructor(props) {
 	// 	super(props);
 	// 	this.state = {
@@ -44,59 +46,59 @@ function LoginForm(props) {
 	useEffect(() => {
 		const cookieObject = hasCookie();
 		if (cookieObject.haslogin) {
-		  setUser({
-			...cookieObject
-		  });
+			setUser({
+				...cookieObject
+			});
 		}
 	}, []);
 
 	async function login(response) {
 		console.log(response.profileObj)
 		if (response.accessToken) {
-		  setUser({
-			...response.profileObj,
-			haslogin: true,
-			accessToken: response.accessToken
-		  })
-		  let res = await axios.get("http://localhost:3001/researcher")
-		  // console.log(res)
-		  let data = res.data
-	
-		  let featuresWeek = null
-		  if (data.length>0){
-			let time =new Date(data[0].time).getTime()
-			console.log(time)
-			if(today-time>=bucketDay){
-			  featuresWeek = await pullFromApi(response,"day", bucketDay, today-time,today)
+			setUser({
+				...response.profileObj,
+				haslogin: true,
+				accessToken: response.accessToken
+			})
+			let res = await axios.get("http://localhost:3001/researcher")
+			// console.log(res)
+			let data = res.data
+
+			let featuresWeek = null
+			if (data.length > 0) {
+				let time = new Date(data[0].time).getTime()
+				console.log(time)
+				if (today - time >= bucketDay) {
+					featuresWeek = await pullFromApi(response, "day", bucketDay, today - time, today)
+				}
+			} else {
+				let start = today - bucketWeek
+				featuresWeek = await pullFromApi(response, "day", bucketDay, start, today)
+				console.log(featuresWeek)
 			}
-		  }else{
-			let start=today-bucketWeek
-			featuresWeek = await pullFromApi(response,"day",bucketDay, start,today)
-			console.log(featuresWeek)
-		  }
-		  if(featuresWeek){
-			await axios.post("http://localhost:3001/researcher", {"featuresWeek":featuresWeek, "googleid":response.profileObj}); 
-		  }
-		  const nearestMeeting = await getElderlyNearestMeeting(response.profileObj.givenName);
-		  props.history.push('/elderly', nearestMeeting);
-		
+			if (featuresWeek) {
+				await axios.post("http://localhost:3001/researcher", { "featuresWeek": featuresWeek, "googleid": response.profileObj });
+			}
+			const nearestMeeting = await getElderlyNearestMeeting(response.profileObj.givenName);
+			props.history.push('/elderly', nearestMeeting);
+
 			SetCookie({
-			...response.profileObj,
-			accessToken: response.accessToken
+				...response.profileObj,
+				accessToken: response.accessToken
 			});
-		}else{
+		} else {
 			setMess('לא ניתן להתחבר עם גוגל. תתחבר עם שם משתמש וסיסמא');
 			// setModal(true);
 		}
 
-		
+
 	}
 
 	function logout(response) {
 		setUser({ haslogin: false, accessToken: '' });
 		DeleteCookie(['accessToken', 'email', 'givenName', 'familyName', 'imageUrl', 'name', 'googleId']);
 	}
-	
+
 	function handleLoginFailure(response) {
 		alert('Failed to log in')
 	}
@@ -104,16 +106,16 @@ function LoginForm(props) {
 		alert('Failed to log out')
 	}
 
-	const getElderlyNearestMeeting=async(userName)=> {
+	const getElderlyNearestMeeting = async (userName) => {
 		let meetings = await fetchMeetingsFullDetails(userName, userTypes.elderly);
 		// meetings = filterMeetings(meetings);
-		if (meetings.length>0){
-			meetings= meetings.reduce((prev, curr) => (prev[meetingFields.meetingDayAndHour] < curr[meetingFields.meetingDayAndHour] ? prev : curr));
+		if (meetings.length > 0) {
+			meetings = meetings.reduce((prev, curr) => (prev[meetingFields.meetingDayAndHour] < curr[meetingFields.meetingDayAndHour] ? prev : curr));
 		}
 		return meetings[0]
 	}
-	
-	const forgotPassword =()=>{
+
+	const forgotPassword = () => {
 		props.history.push('/user/forgot-password');
 	}
 
@@ -121,7 +123,7 @@ function LoginForm(props) {
 		setModal(!modal);
 	}
 
-	const checkOnSubmit = async()=> {
+	const checkOnSubmit = async () => {
 		try {
 
 			const user = await loginCheck(document.getElementById('username').value, document.getElementById('password').value);
@@ -133,12 +135,13 @@ function LoginForm(props) {
 				Cookies.set(usersFields.username, user[usersFields.username]);
 				getCurrentWebSocket();
 				//complete with cookie
-				props.history.push('/elderly');
+				setElderly(true);
+				//props.history.push('/elderly');
 			}
 			else if (user[usersFields.role] === UserRole.Responsible) {
 				props.history.push(`/${UserRole.Responsible}`);
 			}
-			else if (user[usersFields.role] === 'researcher'){
+			else if (user[usersFields.role] === 'researcher') {
 				props.history.push('/researcher');
 			}
 			else {
@@ -149,52 +152,32 @@ function LoginForm(props) {
 			Cookies.set(usersFields.organization, user[usersFields.organization]);
 		}
 		catch (error) {
-			setMess({message: error.message});
+			setMess({ message: error.message });
 			toggleModal();
 		}
 	}
-	// return (
-    //     <div dir="rtl" className="shadow-box">
-    //         <div style={{ position:"relative", right:"40%", top:"60px"}}>
-    //             {user.haslogin ?
-    //             <GoogleLogout
-    //                 clientId={CLIENT_ID}
-    //                 buttonText='Logout'
-    //                 onLogoutSuccess={logout}
-    //                 onFailure={handleLogoutFailure}
-    //             >
-    //             </GoogleLogout> : <GoogleLogin
-    //                 clientId={CLIENT_ID}
-    //                 buttonText='Login'
-    //                 onSuccess={login}
-    //                 onFailure={handleLoginFailure}
-    //                 cookiePolicy={'single_host_origin'}
-    //                 responseType='code,token'
-    //                 scope = { 'https://www.googleapis.com/auth/fitness.activity.read https://www.googleapis.com/auth/fitness.location.read'}
-    //             />
-    //             }
-    //         </div>
-    //     </div>
-	// );
 
 	return (
-		<div className="login-wrapper">
+		<div className="HomePage">
+			<img style={{width:"50%"}} src={images} alt=""/>
+			<div className="login-wrapper">
 			<div className="shadow-box">
 				<div className="form-group">
 					<h2>התחברות</h2>
-					<br/>
 					<label>שם משתמש</label>
-					<input type="text" id="username"/>
+					<input type="text" id="username" />
 					<label>סיסמה</label>
-					<input type="password" id="password"/>
+					<input type="password" id="password" />
 					<div className="align-right">
 						{/* FIXME: */}
 						{/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
 						<a className="forgot-password" href="" onClick={forgotPassword}>שכחתי סיסמה</a>
 					</div>
-					<button className="sb-btn" type="button" onClick={checkOnSubmit}>כניסה</button><br /><br/><label style={{position:"relative", right:"50%"}}>או</label><br />
-					<div >
-								{user.haslogin ?
+					<button className="sb-btn" type="button" onClick={checkOnSubmit}>כניסה</button><br />
+						{isElderly &&
+						<div >
+							<br /><label style={{ position: "relative", right: "30%" }}>אנא אשר באמצעות גוגל</label><br />
+							{user.haslogin ?
 								<GoogleLogout
 									clientId={CLIENT_ID}
 									buttonText='Logout'
@@ -208,18 +191,21 @@ function LoginForm(props) {
 									onFailure={handleLoginFailure}
 									cookiePolicy={'single_host_origin'}
 									responseType='code,token'
-									scope = { 'https://www.googleapis.com/auth/fitness.activity.read https://www.googleapis.com/auth/fitness.location.read'}
+									scope={'https://www.googleapis.com/auth/fitness.activity.read https://www.googleapis.com/auth/fitness.location.read'}
 								/>}
 							{modal ?
-							<Modal
-								{...modal}
-								closeModal={toggleModal}
-							/>
-							: null
+								<Modal
+									{...modal}
+									closeModal={toggleModal}
+								/>
+								: null
 							}
 						</div>
+					}
 				</div>
 			</div>
+		</div>
+		
 		</div>
 	);
 }
