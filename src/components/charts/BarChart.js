bdcc921ea0217b01370890573e60d18d0adc210d
gonend <48886619+gonend@users.noreply.github.com>
2022-06-10 12:22:34 +0300

    random colors added

--- src/components/charts/BarChart.js
+++ src/components/charts/BarChart.js
@@ -1,39 +1,83 @@
 import React from "react";
 
-
 import { Bar, Bubble,Doughnut,Pie,Line,Scatter, } from 'react-chartjs-2';
 import Chart from 'chart.js/auto';
 import Sidebar from "../sidebar/Sidebar";
 
-const BarChart = () => {
-    let state ={
-        labels: ['30','2','10','12','41','12','68','96','11','5'],
-        datasets:[{
-            label:'Rainfall',
-            backgroundColor: ['rgba(75,192,192,1)','rgba(192,75,192,1)','rgba(192,192,75,1)'],
-            borderColor: 'rgba(0,0,0,1)',
-            borderWidth:2,
-            data:[30,2,10,12,41,12,68,96,11,5]
-        }]
+const BarChart = (props) => {
+    const backgroundColors =[]
+    function getRandomColor() {
+        var letters = '0123456789ABCDEF'.split('');
+        var color = '#';
+        for (var i = 0; i < 6; i++ ) {
+            color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color;
+            }
+    const getBackgroundColors = () =>{
+        for (var i=0;i<props.labels.length; i++){
+            backgroundColors.push(getRandomColor());
+        }
+        return backgroundColors;
     }
-
-    return(<div className="charts">
-    <Bar style={{textAlign:'center'}}
-        data={state}
-        options= {{
-            title:{
-                display: true,
-                text:'Average Rainfall per month',
-                fontSize:20
-            },
-            legend:{
-                display:true,
-                position:'right'
-            },
-        }}
-        />
-        </div>
-    );
+    let state = {
+        labels: props.labels,
+        datasets: [
+            {
+                label: props.label,
+                fill: true,
+                backgroundColor: getBackgroundColors(),
+                borderColor: 'rgba(0,0,0,0.9)',
+                borderWidth: 2,
+                data: props.data
+            }
+        ]
+    }
+    return(
+    <div className="charts">
+    <Bar style={{ textAlign: 'center' }}
+                        data={state}
+                        options={{
+                            title: {
+                                display: true,
+                                fontSize: 20
+                            },
+                            legend: {
+                                display: true,
+                                position: 'right'
+                            },
+                            scales: {
+                                y:
+                                {
+                                    min: 0,
+                                    max: 100,
+                                    stepSize: 1,
+                                },
+                            }
+                        }}
+                    />
+                    <Line style={{ textAlign: 'center' }}
+                        data={state}
+                        options={{
+                            title: {
+                                display: true,
+                                fontSize: 20
+                            },
+                            legend: {
+                                display: true,
+                                position: 'right'
+                            },
+                            scales: {
+                                y:
+                                {
+                                    min: 0,
+                                    max: 100,
+                                    stepSize: 1,
+                                },
+                            }
+                        }} />
+    </div>
+        );
 
 }
 export default BarChart;
--- src/components/pages/ResearcherPage.js
+++ src/components/pages/ResearcherPage.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 import { Bar, Line } from 'react-chartjs-2';
 import Chart from 'chart.js/auto';
 import { serverURL } from '../../ClientUtils'
-
+import BarChart from '../charts/BarChart';
 
 
 function ResearcherPage(props) {
@@ -12,77 +12,57 @@ function ResearcherPage(props) {
     // const [showLine, setShowLine] = useState(false);
     const [data, setData] = useState([]);
     const [label, setLabel] = useState('');
-    const [labels, setLabels] = useState([]);
+    const [labels, setLabels] = useState(['1','2','3','4','5','6','7']);
 
 
-    const [start, setStart] = useState(Date);
-    const [end, setEnd] = useState(Date);
+    const [start, setStart] = useState();
+    const [end, setEnd] = useState();
 
-    let state = {
-        labels: labels,
-        datasets: [
-            {
-                label: label,
-                fill: true,
-                backgroundColor: ['rgba(75,192,192,0.3)', 'rgba(192,75,192,0.3)', 'rgba(192,192,75,0.3)'],
-                borderColor: 'rgba(0,0,0,0.9)',
-                borderWidth: 2,
-                data: data
-            }
-        ]
+    function changeLable(newLabel){
+        setLabel(prev => prev = newLabel);   
     }
 
     // useEffect(() => {
     //     if ()
     // });
 
-    const setLabelsValue = (array) => {
-        let arr = []
-        for (let index = 0; index < array.length; index++) {
-            arr.push(`day ${index}`);
-        }
-        setLabels(arr)
-    }
     const getFeatures = async () => {
-        return await axios.get(`${serverURL}/researcher/features/${start}/${end}`);
+        return await axios.get(`${serverURL}/researcher/features`);
     }
 
     const showSteps = async (e) => {
-
+        
         let response = [10, 20, 45, 30, 55, 70, 60]//await getFeatures();
         // let response = await getFeatures();
-        setLabel('Steps');
+  
+        
         setData(response);
-        setLabelsValue(response);
         (showBar) ? setShowBar(false) : setShowBar(true);
+        setLabel('Steps');
     }
     const showHR = async () => {
         let response = await getFeatures();
         setData(response.data.HR);
-        setLabelsValue(response.data.HR);
         setLabel('HR');
         (showBar) ? setShowBar(false) : setShowBar(true);
     }
     const showDistance = async () => {
         let response = await getFeatures();
         setData(response.data.Distance);
-        setLabelsValue(response.data.Distance);
-        setLabel('Distance');
-        (showBar) ? setShowBar(false) : setShowBar(true);
+        setLabel('Distance')
+            (showBar) ? setShowBar(false) : setShowBar(true);
     }
     const showAM = async () => {
         let response = await getFeatures();
         setData(response.data.Active_min);
-        setLabelsValue(response.data.Active_min);
-        setLabel('Active Minutes');
-        (showBar) ? setShowBar(false) : setShowBar(true);
+        setLabel('Active Minutes')
+            (showBar) ? setShowBar(false) : setShowBar(true);
     }
     const showSleep = async () => {
         let response = await getFeatures();
         setData(response.data.Sleep);
-        setLabelsValue(response.data.Sleep);
-        setLabel('Hour of Sleeps');
-        (showBar) ? setShowBar(false) : setShowBar(true);
+        setLabel('Hour of Sleeps')
+            (showBar) ? setShowBar(false) : setShowBar(true);
 
     }
 
@@ -119,59 +99,19 @@ function ResearcherPage(props) {
     );
 
     return (
-        <div>
-            <div >
-                <Sidebar history={props.history} content={content} />
-            </div>
-
-            <div style={{ position: 'absolute', top: 20, left: '40%', width: '200px', backgroundColor: 'lightcyan' }}>
-                <input type='date' className='start' value={start} onChange={e => setStart(e.target.value)} />
-                <input type='date' className='end' value={end} onChange={e => setEnd(e.target.value)} />
-
+        <div className="page">
+            <Sidebar history={props.history} content={content} />
+            
+            
+            <div style={{width:'30%'}}>
+                <input  type='date' className='start' value={start} onChange={e=>setStart(e.target.value)}/>
+                <input type='date' className='end' value={end} onChange={e=>setEnd(e.target.value)} />
             </div>
             {showBar &&
                 <div style={{ position: "absolute", top: '100px', left: '25%', height: '50%', width: '40%', backgroundColor: 'white' }}>
-                    <Bar style={{ textAlign: 'center' }}
-                        data={state}
-                        options={{
-                            title: {
-                                display: true,
-                                fontSize: 20
-                            },
-                            legend: {
-                                display: true,
-                                position: 'right'
-                            },
-                            scales: {
-                                y:
-                                {
-                                    min: 0,
-                                    max: 100,
-                                    stepSize: 1,
-                                },
-                            }
-                        }}
-                    />
-                    <Line style={{ textAlign: 'center' }}
-                        data={state}
-                        options={{
-                            title: {
-                                display: true,
-                                fontSize: 20
-                            },
-                            legend: {
-                                display: true,
-                                position: 'right'
-                            },
-                            scales: {
-                                y:
-                                {
-                                    min: 0,
-                                    max: 100,
-                                    stepSize: 1,
-                                },
-                            }
-                        }} />
+                    
+                    <BarChart data ={data} label={label} labels = {['1','2','3','4','5','6','7',]}/>
+                    
                 </div>
 
             }
